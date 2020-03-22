05cdb2db76c2476d3e1b882080e3c1eb75a65823
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-03-22 16:04:38 -0700

    log in finished

--- Secrets/app.js
+++ Secrets/app.js
@@ -32,11 +32,30 @@ app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.route("/register")
-.post(function(req, res) {
-  const newUser = new User ({
-    email : req.body.username,
-    password : req.body.password
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets');
+        }
+      }
+    }
+  });
+});
+
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
   });
 
   newUser.save(function(err) {
@@ -46,8 +65,9 @@ app.route("/register")
       res.send(err);
     }
   });
-})
-.get(function(req, res) {
+});
+
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
