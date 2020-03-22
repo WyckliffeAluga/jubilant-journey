e9c422acb97077bef9b4543d506cc0b957ac1404
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-03-22 15:13:26 -0700

    mongoose

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,1 +1,57 @@
-//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.route("/register")
+.post(function(req, res) {
+  const newUser = new User ({
+    email : req.body.username,
+    password : req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      res.send(err);
+    }
+  });
+})
+.get(function(req, res) {
+  res.render("register");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.")
+});
