c9cddd421872feeb0085b062753f124e2ee68179
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-03-22 23:45:41 -0700

    level 4 security

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,6 +4,10 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,7 +28,7 @@ const userSchema = mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -48,9 +52,11 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+              res.render('secrets');
+          }
+        });
       }
     }
   });
@@ -58,19 +64,24 @@ app.post("/login", function(req, res) {
 
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
 
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      res.send(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
   });
 });
+
 
 app.get("/register", function(req, res) {
   res.render("register");
