1217c821714ce6826dbfcdf4fbd3d25aa451ca0b
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-03-29 19:07:42 -0700

    added mapping of notes

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note"
+import notes from "../Notes"
+
+function createNotes(note) {
+  return (
+    < Note
+    title = {note.title}
+    content = {note.content}/>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Footer/>
-      <Note/>
-    </div>
+    <Header/>
+    <Footer/>
+   {notes.map(createNotes)}
+   </div>
   )
 }
 
