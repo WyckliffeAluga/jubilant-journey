a88a6a4ddca2b51e3307fc8d57dfc1d30e5e852c
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-04-15 00:12:31 -0700

    final styling

--- keeper/src/components/Header.jsx
+++ keeper/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import NotesIcon from '@material-ui/icons/Notes';
 
 
 function Header() {
   return (
     <header>
-      <h1> Keeper </h1>
+      <h1> <NotesIcon/> Notes </h1>
     </header>
   )
 }
 
-export default Header ; 
+export default Header ;
