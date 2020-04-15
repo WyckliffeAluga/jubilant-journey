292b2d9926efc9f30acef28146afc9368882a8e1
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-04-14 23:26:22 -0700

    keeper app functionality improved

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note"
-import notes from "../Notes"
-
-function createNotes(note) {
-  return (
-    < Note
-    key = {note.key}
-    title = {note.title}
-    content = {note.content}/>
-  )
-}
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, addNotes] = useState([]);
+
+  function addNote(newNote) {
+    addNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    addNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
-    <Header/>
-    <Footer/>
-   {notes.map(createNotes)}
-   </div>
-  )
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
